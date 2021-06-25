a3bde5fadaf9f066cb5db93dffa21eec8876bfe0
masaakiKanno850 <masaaki.kanno.pr@gmail.com>
2021-06-25 15:17:03 +0000

    first commit

---
+++ mentor.rb
@@ -0,0 +1,29 @@
+class Mentor
+    
+    attr_accessor :name,:job
+    
+    #初期化
+    def initialize (name)
+        @name = name 
+    end
+    #仕事を紹介する
+    def job
+      puts "#{@name}です。私は現役のITプロフェッショナルです。"
+    end
+    
+end
+
+class RailsMentor < Mentor
+     
+    #オーバーライドして仕事を紹介する
+    def job
+      puts "#{@name}です。私はRubyとRailsでWebアプリケーションを作ります。"
+    end
+    
+end
+
+kirameki = Mentor.new("煌木")
+kirameki.job
+#RailsMentorクラスでjobをオーバーライドを実行
+akaide = RailsMentor.new("赤出")
+akaide.job
